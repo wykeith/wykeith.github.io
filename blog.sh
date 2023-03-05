0ac905b523d55fec5e67048351b4021d77036351
wykeith <wykeith@gmail.com>
2023-03-05 01:20:31 +0000

    update bash script

--- blog.sh
+++ blog.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-source /home/nbuser/anaconda3_420/bin/activate
+#source /home/nbuser/anaconda3_420/bin/activate
 #pip install -r requirements.txt
 pelican content -s publishconf.py
 ghp-import output -b master
 git push origin master
+git config --global user.email "wykeith@gmail.com"
+git config --global user.name "wykeith"
