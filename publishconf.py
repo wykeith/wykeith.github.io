745be2e7969a94974f30390710583fd8a03eeefe
wykeith <wykeith@gmail.com>
2025-03-23 19:57:28 +0800

    Add pingan

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://keith.ml'
+SITEURL = 'https://writing.mlops.im'
 RELATIVE_URLS = True 
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
